8ac4a8bcecb8166eafc60282c9af5d26634e14f1
G0yter <goyter012@gmail.com>
2022-11-23 13:56:38 +0200

    Initial Commit

---
+++ CommentsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CommentsApp
+//
+//  Created by Admin on 23.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
